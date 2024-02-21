30ec371c0713df5d0e3248d72740101a16e3e11b
PierreMerzon <153851795+PierreMerzon@users.noreply.github.com>
2024-02-21 16:20:21 +0100

    fh

--- SupraKill/Assets/SupraKill/Scripts/Finish.cs
+++ SupraKill/Assets/SupraKill/Scripts/Finish.cs
@@ -7,6 +7,8 @@ public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
 
+    private bool levelCompleted = false;
+
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
@@ -14,9 +16,10 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
+            levelCompleted = true;
             //Invoke("CompleteLevel", 1f);
         }
     }
