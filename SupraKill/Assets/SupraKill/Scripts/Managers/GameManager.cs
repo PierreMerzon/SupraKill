8f0152d88520b83be813bd7c6d014692f873cd1d
PierreMerzon <153851795+PierreMerzon@users.noreply.github.com>
2024-02-26 00:26:15 +0100

    gdfg

--- SupraKill/Assets/SupraKill/Scripts/Managers/GameManager.cs
+++ SupraKill/Assets/SupraKill/Scripts/Managers/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Inicio del singleton básico
     private static GameManager instance;
     public static GameManager Instance
     {
@@ -17,11 +18,27 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //Fin del Singleton básico, se referencia en el awake
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //Variables
+
     [Header("General Data")]
+    public int points;
+    public int winPoints;
     public int score;
     public int maxScore;
     public int health;
     public int energy;
+
+    public void PointsUp(int gain)
+    {
+        points += gain;
+    }
 
     [Header("Game Status")]
     public bool gameCompleted = false;
